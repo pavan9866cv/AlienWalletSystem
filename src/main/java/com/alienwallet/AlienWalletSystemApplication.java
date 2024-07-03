5f44541ae87d0ff4d6a5e646ad8067429f354e23
Venkata Pavan Sai Teja Chenna <pavanchenna1999@gmail.com>
2024-07-03 13:46:56 +0530

    Initial Commit

---
+++ src/main/java/com/alienwallet/AlienWalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alienwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienWalletSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alienwallet/AlienWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alienwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
